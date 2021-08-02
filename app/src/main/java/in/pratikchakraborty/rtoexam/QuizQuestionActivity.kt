c65d3deb23a01fcab9345dfc5966ec4b7218ed77
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-08-02 16:59:56 +0530

    StatusBar and ActionBar Color Update for MockTest

--- app/src/main/java/in/pratikchakraborty/rtoexam/QuizQuestionActivity.kt
+++ app/src/main/java/in/pratikchakraborty/rtoexam/QuizQuestionActivity.kt
@@ -3,9 +3,12 @@ package `in`.pratikchakraborty.rtoexam
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
+import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
@@ -19,6 +22,15 @@ class QuizQuestionActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_question)
+        // Color Change for Support Action Bar
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.dark_violet)))
+        // Color Change for Status Bar
+        if (Build.VERSION.SDK_INT >= 21) {
+            val window = this.window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            window.statusBarColor = this.resources.getColor(R.color.dark_violet)
+        }
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         mQuestionsList = Constants.getQuestions()
         setQuestion()
