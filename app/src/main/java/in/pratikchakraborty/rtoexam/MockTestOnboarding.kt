c76ee711b728f96b3946527153479a71b01ed5ea
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-08-02 16:35:04 +0530

    Questions Setup Successfully

--- app/src/main/java/in/pratikchakraborty/rtoexam/MockTestOnboarding.kt
+++ app/src/main/java/in/pratikchakraborty/rtoexam/MockTestOnboarding.kt
@@ -1,10 +1,12 @@
 package `in`.pratikchakraborty.rtoexam
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_mock_test_onboarding.*
 
 class MockTestOnboarding : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +20,10 @@ class MockTestOnboarding : AppCompatActivity() {
         window?.decorView?.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
         window.statusBarColor = Color.TRANSPARENT
+        btn_start.setOnClickListener {
+            val intent = Intent(this, QuizQuestionActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
