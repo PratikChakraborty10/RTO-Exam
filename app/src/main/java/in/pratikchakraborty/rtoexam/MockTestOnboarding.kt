e634dadd673797c2692c5f2d9929e254829bbdeb
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-08-02 22:58:06 +0530

    User Prohibitted from starting quiz, without entering name

--- app/src/main/java/in/pratikchakraborty/rtoexam/MockTestOnboarding.kt
+++ app/src/main/java/in/pratikchakraborty/rtoexam/MockTestOnboarding.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_mock_test_onboarding.*
 
@@ -21,10 +22,14 @@ class MockTestOnboarding : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
         window.statusBarColor = Color.TRANSPARENT
         btn_start.setOnClickListener {
-            val intent = Intent(this, QuizQuestionActivity::class.java)
-            intent.putExtra(Constants.USER_NAME, et_name.text.toString())
-            startActivity(intent)
-            finish()
+            if(et_name.text.toString().isEmpty()) {
+                Toast.makeText(this, "To start the test, you will have to enter your name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                intent.putExtra(Constants.USER_NAME, et_name.text.toString())
+                startActivity(intent)
+                finish()
+            }
         }
     }
 }
