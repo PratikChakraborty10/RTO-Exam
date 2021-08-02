ab45f006767da566cdc3dc0427ebda68abcda0f1
PratikChakraborty10 <chakrabortypratik2000@gmail.com>
2021-08-03 01:05:05 +0530

    Mocktest Onboarding toast changed to snackbar

--- app/src/main/java/in/pratikchakraborty/rtoexam/MockTestOnboarding.kt
+++ app/src/main/java/in/pratikchakraborty/rtoexam/MockTestOnboarding.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_mock_test_onboarding.*
 
 class MockTestOnboarding : AppCompatActivity() {
@@ -23,7 +25,15 @@ class MockTestOnboarding : AppCompatActivity() {
         window.statusBarColor = Color.TRANSPARENT
         btn_start.setOnClickListener {
             if(et_name.text.toString().isEmpty()) {
-                Toast.makeText(this, "To start the test, you will have to enter your name", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "To start the test, you will have to enter your name", Toast.LENGTH_SHORT).show()
+                Snackbar.make(it, "To start the test, you wil have to enter your name", Snackbar.LENGTH_LONG)
+                    .setAnimationMode(BaseTransientBottomBar.ANIMATION_MODE_FADE)
+                    .setBackgroundTint(Color.parseColor("#2a0163"))
+                    .setAction("ENTER NAME") {
+                        val intent = Intent(this, MockTestOnboarding::class.java)
+                        startActivity(intent)
+                    }
+                    .show()
             } else {
                 val intent = Intent(this, QuizQuestionActivity::class.java)
                 intent.putExtra(Constants.USER_NAME, et_name.text.toString())
